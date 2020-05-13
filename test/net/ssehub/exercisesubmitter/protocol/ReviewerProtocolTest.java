0b5a7f6d5f8faa331c01a3062f1b4835dafa2f93
TimKuno <tim.kunold@gmx.de>
2020-05-13 12:54:43 +0200

    added test case for getAssessmentForAssignment

--- test/net/ssehub/exercisesubmitter/protocol/ReviewerProtocolTest.java
+++ test/net/ssehub/exercisesubmitter/protocol/ReviewerProtocolTest.java
@@ -20,6 +20,7 @@ public class ReviewerProtocolTest {
     public static final String TEST_SERVER = "http://147.172.178.30:3000";
     public static final String TEST_COURSE_ID = "java";
     public static final String TEST_ASSIGNMENT_ID = "993b3cd0-6207-11ea-bc55-0242ac130003";
+    public static final String TEST_ASSESSMENT_ID = "680dd44a-93b0-4d1c-a947-9b50a4bbb68e";
     public static final String TEST_SEMESTER = "wise1920";
     
     /**
@@ -37,4 +38,20 @@ public class ReviewerProtocolTest {
             Assert.fail("Unexpected NetworkException returned: " + e.getMessage());
         }
     }
+    
+    /**
+     * Tests if a single Assessment for an Assignment is returned.
+     */
+    @Test
+    public void testGetAssessmentForAssignment() {
+        ReviewerProtocol rp = new ReviewerProtocol(TEST_SERVER, TEST_COURSE_ID);
+        rp.setSemester(TEST_SEMESTER);
+        try {
+            AssessmentDto assessment = rp.getAssessmentForAssignment(TEST_ASSIGNMENT_ID, TEST_ASSESSMENT_ID);
+            Assert.assertNotNull("No Assessment returned", assessment);
+        } catch (NetworkException e) {
+            Assert.fail("Unexpected NetworkException returned: " + e.getMessage());
+        }
+    }
+    
 }
